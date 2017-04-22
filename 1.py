4a50b3a12fea173b72b9f7484c48e81d65edb810
indrajit <eendroroy@gmail.com>
2017-04-22 23:52:22 +0600

    refactor

--- 1.py
+++ 1.py
@@ -1,7 +1,3 @@
 # https://projecteuler.net/problem=1
 
-res = 0
-for i in range(1, 1000):
-    if i % 3 == 0 or i % 5 == 0:
-        res += i
-print res
+print sum(i if i % 3 == 0 or i % 5 == 0 else 0 for i in range(1, 1000))
