5757fdebce6df72327d4bae28d8fb8f0ee6e01f2
indrajit <eendroroy@gmail.com>
2017-04-24 00:21:32 +0600

    triangle

--- solved/l/triangle.py
+++ solved/l/triangle.py
@@ -3,5 +3,12 @@ def triangle(limit):
     index = 1
     while triangle_nums[-1] < limit:
         triangle_nums.append(triangle_nums[index-1] + index + 1)
+        index += 1
+
+    if triangle_nums[-1] > limit:
+        triangle_nums.pop()
 
     return triangle_nums
+
+if __name__ == '__main__':
+    print triangle(100)
